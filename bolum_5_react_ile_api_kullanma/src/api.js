77e266bb9f927c5f893db7b255da27b358949f2a
Ahmet Arif AYDIN <ahmetarifaydina@gmail.com>
2024-11-08 18:23:53 +0300

    bolum_5

--- bolum_5_react_ile_api_kullanma/src/api.js
+++ bolum_5_react_ile_api_kullanma/src/api.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const searchImages = async (term) => {
-  const response = await axios.get('https://api.unsplash.com/search/photos', {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
-      Authorization: 'Client-ID 8O50V7bNzfKdVixwS9W9nZVdr0VnrCv9gmeimfdvp6Y',
+      Authorization: "Client-ID HlvtLijcsfPBWqJ3J4h7XO9-9ePad4_lDFyAAhvmlfo",
     },
     params: {
       query: term,
